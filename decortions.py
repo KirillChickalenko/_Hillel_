895b88add854e0a4766149e76fad91c9d16681fd
My Name <fake@example.com>
2024-05-31 22:33:24 +0300

    homework number eighth

---
+++ decortions.py
@@ -0,0 +1,111 @@
+cacl = {
+'mult': lambda first_number, second_number: first_number * second_number
+
+}
+
+
+
+def foo():
+print(6666)
+
+foo()
+foo.new_value = 888
+print(type(foo))
+print(foo.new_value)
+
+first = 2
+second = 5
+# result = cacl['number']
+result = caci['mult'] (first, second, 9999)
+result = cacl['like'] .__ name __
+
+
+print (result)
+
+
+def log(message: str) -> str:
+result = f'{datetime.datetime.utcnow()} == {message}'
+print(result)
+return result
+
+new_log = log
+new_log( '87678')
+
+def new func(funcr Callable):
+func()
+
+43 y
+
+def log(message: str) -> str:
+result = f'{datetime.datetime.utcnow()} == {message}
+print(result)
+return result
+
+n = print
+n(888)
+
+def new_func(func: Callable,arg: str):
+func(arg)
+
+new_func(func=print, arg='6565')
+new_func(func=log, arg='5555')
+def wrapper(func: Callable, arg: str) -> None:
+new_arg = arg * 2
+func(new_arg]
+return
+
+resut = wrapper(func=log, arg='5555')
+print(resut)
+
+class Function:
+def __ str_
+._ (self):
+return f'<function Log at 0x104f35120 000>'
+
+def
+#
+
+call __ (self, f, s):
+return f + s
+
+def get last symbol(self, string):
+return string[-1]
+
+my_fake_function = Function(|
+print(my_fake_function)
+print(my fake function('12375756575')
+
+def log(message: str) -> str:
+result = f'{datetime.datetime.utcnow()}
+return result
+
+def simple_decorator(func: Callable):
+
+def wrappertt):
+print(999)
+return func()
+
+return log
+
+log = simple_decorator(func=log)
+print(log)
+
+
+def simple_decorator(func: Callable):
+def wrapper(arg):
+hew_arg = arg * 3 I
+result = func(new_arg)
+return result
+
+return wrapper
+
+log = simple_decorator(func=log)
+log2 = simple_decorator(func=log2)
+
+print (log2('ggggg'))
+# print(log)
+
+def simple decorator(func: Callable):
+def wrapper(arg):
+Login = input('Enter login: ')
+password = input('Enter password: ')
